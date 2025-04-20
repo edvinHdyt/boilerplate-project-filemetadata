1189ae7baef5a3604d53d4561221fe3b419e2fbb
vI <79068692+edvinHdyt@users.noreply.github.com>
2025-04-20 14:23:42 +0000

    ~

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var cors = require('cors');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 require('dotenv').config()
 
 var app = express();
 
+app.use(bodyParser.urlencoded());
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -12,6 +16,10 @@ app.get('/', function (req, res) {
 });
 
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  // console.log(req.file);
+  res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+})
 
 
 const port = process.env.PORT || 3000;
